796061a16092ae2aeb8460f5cb76754754010ff1
Biser Karadzhov <85568905+Digital-Human-BK@users.noreply.github.com>
2023-01-20 11:58:49 +0200

    [add] UserImage component

---
+++ src/components/UserImage.jsx
@@ -0,0 +1,18 @@
+import Box from "@mui/material/Box";
+
+// eslint-disable-next-line react/prop-types
+function UserImage({ image, size = "60px" }) {
+  return (
+    <Box width={size} height={size}>
+      <img
+        style={{ objectFit: "cover", borderRadius: "50%" }}
+        width={size}
+        height={size}
+        alt="user"
+        src={`https://good-puce-scorpion-kilt.cyclic.app/assets/${image}`}
+      />
+    </Box>
+  );
+}
+
+export default UserImage;
