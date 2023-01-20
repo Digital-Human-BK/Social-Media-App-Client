ed6a2119a26fdb88666306ee6cf0a5304705d800
Biser Karadzhov <85568905+Digital-Human-BK@users.noreply.github.com>
2023-01-20 13:15:18 +0200

    [add] User widget into the Home page

--- src/scenes/homePage/index.jsx
+++ src/scenes/homePage/index.jsx
@@ -1,10 +1,29 @@
-import { Box } from "@mui/material";
+import { useSelector } from "react-redux";
+
+import Box from "@mui/material/Box";
+import { useMediaQuery } from "@mui/material";
+
 import Navbar from "../navbar";
+import { selectUser } from "../../state";
+import UserWidget from "../widgets/UserWidget";
 
 function HomePage() {
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const { _id, picturePath } = useSelector(selectUser);
   return (
     <Box>
       <Navbar />
+      <Box
+        width="100%"
+        p="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+      </Box>
     </Box>
   );
 }
