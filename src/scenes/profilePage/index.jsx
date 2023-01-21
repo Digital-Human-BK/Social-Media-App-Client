042bfcabe8392aae472fbb920ed49c9429eac549
Biser Karadzhov <85568905+Digital-Human-BK@users.noreply.github.com>
2023-01-21 22:22:45 +0200

    [add] Profile page

--- src/scenes/profilePage/index.jsx
+++ src/scenes/profilePage/index.jsx
@@ -1,5 +1,72 @@
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import Box from "@mui/material/Box";
+import { useMediaQuery } from "@mui/material";
+
+import Navbar from "../navbar";
+import UserWidget from "../widgets/UserWidget";
+import PostsWidget from "../widgets/PostsWidget";
+import MyPostWidget from "../widgets/MyPostWidget";
+import FriendListWidget from "../widgets/FriendListWidget";
+
+import { selectToken } from "../../state";
+
 function ProfilePage() {
-  return <div>Profile page</div>;
+  const { userId } = useParams();
+  const [user, setUser] = useState(null);
+  const token = useSelector(selectToken);
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+
+  const getUser = async () => {
+    const response = await fetch(
+      `https://good-puce-scorpion-kilt.cyclic.app/users/${userId}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const userData = await response.json();
+    setUser(userData);
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  if (!user) {
+    return null;
+  }
+  return (
+    <Box>
+      <Navbar />
+      <Box
+        width="100%"
+        p="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="2rem"
+        justifyContent="center"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={userId} picturePath={user.picturePath} />
+          <Box m="2rem 0" />
+          <FriendListWidget userId={userId} />
+        </Box>
+
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={user.picturePath} />
+          <Box m="2rem 0" />
+          <PostsWidget userId={userId} isProfile />
+        </Box>
+      </Box>
+    </Box>
+  );
 }
 
 export default ProfilePage;
